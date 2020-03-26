bd5c7fa5240084abe356a13ad9b9729263421799
NickOfRhyme <nhwoolrich@gmail.com>
2020-03-26 10:00:58 +0000

    Solved kata

--- herd-the-babies.js
+++ herd-the-babies.js
@@ -1,3 +1,43 @@
-const herdTheBabies = () => {};
+const herdTheBabies = (str = "") => {
+  const alphabetisedStr = alphabetise(str);
+  return groupLetters(alphabetisedStr)
+    .map(letterGroup => arrangeCases(letterGroup))
+    .join("");
+};
 
+const alphabetise = str => {
+  return str
+    .split("")
+    .sort(
+      (a, b) => a.toLowerCase().charCodeAt(0) - b.toLowerCase().charCodeAt(0)
+    )
+    .join("");
+};
+
+const groupLetters = str => {
+  let currLetterGroup = "";
+  let letterArr = [];
+  str.split("").forEach((letter, idx) => {
+    if (idx === 0 || str[idx - 1].toLowerCase() === letter.toLowerCase()) {
+      currLetterGroup = currLetterGroup.concat(letter);
+    } else {
+      letterArr.push(currLetterGroup);
+      currLetterGroup = letter;
+    }
+    if (idx === str.length - 1) {
+      letterArr.push(currLetterGroup);
+      currLetterGroup = letter;
+    }
+  });
+  return letterArr;
+};
+
+const arrangeCases = str => {
+  return str
+    .split("")
+    .sort((a, b) => {
+      return a.charCodeAt(0) - b.charCodeAt(0);
+    })
+    .join("");
+};
 module.exports = herdTheBabies;
--- spec/herd-the-babies.spec.js
+++ spec/herd-the-babies.spec.js
@@ -1,2 +1,35 @@
 const { expect } = require("chai");
 const herdTheBabies = require("../herd-the-babies");
+
+describe("herdTheBabies", () => {
+  it("when passed nothing or an empty string, returns an empty string", () => {
+    const input = "";
+    let actual = herdTheBabies(input);
+    const expected = "";
+    expect(actual).to.equal(expected);
+    actual = herdTheBabies();
+    expect(actual).to.equal(expected);
+  });
+  it("when passed a string of letters of only one case, returns those letters in alphabetical order", () => {
+    let input = "bcfedsa";
+    let actual = herdTheBabies(input);
+    let expected = "abcdefs";
+    expect(actual).to.equal(expected);
+    input = "BCFEDSA";
+    actual = herdTheBabies(input);
+    expected = "ABCDEFS";
+    expect(actual).to.equal(expected);
+  });
+  it("when passed a string consisting of the same letter multiple times but in mixed cases, returns the string with the upper-case letters moved to the front", () => {
+    const input = "AaAaA";
+    const actual = herdTheBabies(input);
+    const expected = "AAAaa";
+    expect(actual).to.equal(expected);
+  });
+  it("when passed a string consisting of several different letters and a mix of cases, alphabetises the string and moves upper-case letters to the start of their letter groups", () => {
+    const input = "AAsdsazZzyXxX";
+    const actual = herdTheBabies(input);
+    const expected = "AAadssXXxyZzz";
+    expect(actual).to.equal(expected);
+  });
+});
